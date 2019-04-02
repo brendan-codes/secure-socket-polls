4a2d52ed54cf0feea451169f401b47f12d543be8
Brendan Stanton <bmsenate@gmail.com>
2019-04-01 17:51:44 -0700

    added cronjob

--- server/controllers/socket-controller.js
+++ server/controllers/socket-controller.js
@@ -1,25 +1,42 @@
+let mongoose = require('mongoose'),
+    cron = require('node-cron'),
+    Poll = mongoose.model('Poll');
 
 
+let TOTALS = {
+    yay: 0,
+    nay: 0,
+    overflow: 0
+};
+
+const LIMITS = {
+    overflow: 255,
+    schedule: '*/5 * * * *'
+};
 
 module.exports = function(io){
 
     io.on('connection', function(socket){
 
-
         console.log('somebody connected!')
 
-
-
         socket.on('nay:vote', function(data){
-            console.log("nay++");
-            // update
+            // update totals
 
+            // broadcast immediately
             socket.broadcast.emit('nay:broadcast', {
                 data: 0
             });
+
+            // udpate overflow
+            TOTALS.overflow++;
+
+            // check overflow
+            checkOverflow();
         });
 
         socket.on('yay:vote', function(data){
+            TOTALS.overflow++;
             console.log("yay++");
             // update
 
@@ -28,17 +45,22 @@ module.exports = function(io){
             });
         });
 
-
-        // welcome to sockets
-
-            // vote yay on poll
-            // vote nay on poll
-
-            // broadcast poll updates
-
-            // fill buffer
-            // buffer overflow control
-
     });
 
 };
+
+function checkOverflow(){
+    if(TOTALS.overflow > LIMITS.overflow){
+        // save operation
+
+        TOTALS.overflow = 0;
+    }
+};
+
+cron.schedule(LIMITS.schedule, () => {
+    // five minute cron
+
+    // save operation
+
+    TOTALS.overflow = 0;
+});
